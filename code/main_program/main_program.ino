bb6fe6b06e92567986ae23173b85b5a2188f47ec
Sashwat K <sashwat0001@gmail.com>
2018-08-26 07:45:35 +0530

    Test

---
+++ code/main_program/main_program.ino
@@ -0,0 +1,29 @@
+/***************************************************************************
+*    ______________            ______ _____       ______________ ______    *
+*  ___  __ )__  /_____ _________  /____(_)________  __ \_  __ \/__  /      *
+*  __  __  |_  /_  __ `/  ___/_  //_/_  /__  ___/  / / /  / / /__  /       *
+*  _  /_/ /_  / / /_/ // /__ _  ,<  _  / _(__  )/ /_/ // /_/ / _  /        *
+*  /_____/ /_/  \__,_/ \___/ /_/|_| /_/  /____/ \____/ \____/  /_/         *
+*                                                                          *
+*                                                                          *
+*  Name: Sashwat K                                                         *               
+*  Created on: 26 August 2018                                              *
+*  Last Modified: 26 August 2018                                           *
+*  Revision: 1                                                             *
+***************************************************************************/
+int l1,l2,l3; //controlling switches
+int s1,s2,s3; //Controlling switches manually
+boolean lb1=0,lb2=0,lb3=0; //for status of the switches
+
+void setup() {
+ pinMode(l1,OUTPUT);
+ pinMode(l2,OUTPUT);
+ pinMode(l3,OUTPUT);
+ pinMode(s1,OUTPUT);
+ pinMode(s2,OUTPUT);
+ pinMode(s3,OUTPUT);
+}
+
+void loop() {
+  
+}
