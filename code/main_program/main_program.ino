fb407f47c9259ba43e0241a9af925bad2a71b8bd
Sashwat K <sashwat0001@gmail.com>
2019-01-08 14:23:32 +0530

    Added new code for particle.io

--- code/main_program/main_program.ino
+++ code/main_program/main_program.ino
@@ -1,29 +1,74 @@
-/***************************************************************************
-*    ______________            ______ _____       ______________ ______    *
-*  ___  __ )__  /_____ _________  /____(_)________  __ \_  __ \/__  /      *
-*  __  __  |_  /_  __ `/  ___/_  //_/_  /__  ___/  / / /  / / /__  /       *
-*  _  /_/ /_  / / /_/ // /__ _  ,<  _  / _(__  )/ /_/ // /_/ / _  /        *
-*  /_____/ /_/  \__,_/ \___/ /_/|_| /_/  /____/ \____/ \____/  /_/         *
-*                                                                          *
-*                                                                          *
-*  Name: Sashwat K                                                         *               
-*  Created on: 26 August 2018                                              *
-*  Last Modified: 26 August 2018                                           *
-*  Revision: 1                                                             *
-***************************************************************************/
-int l1,l2,l3; //controlling switches
-int s1,s2,s3; //Controlling switches manually
-boolean lb1=0,lb2=0,lb3=0; //for status of the switches
+//  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄         ▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄ 
+// ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌
+// ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀▀▀ 
+// ▐░▌          ▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          
+// ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄▄▄ 
+// ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌
+//  ▀▀▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌ ▀▀▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌
+//           ▐░▌▐░▌       ▐░▌          ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌
+//  ▄▄▄▄▄▄▄▄▄█░▌▐░▌       ▐░▌ ▄▄▄▄▄▄▄▄▄█░▌▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌
+// ▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+//  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀ 
+// Name: Sashwat K
+// Code: Home automation using particle.io and IFTTT
+// Created on: 15 Nov 2018
+// Revision: 2
+// Variables used
+// relayTubelight (Tube Light) - integer - D0 (GPIO4)
+// relayBulb (Bulb) - integer - D1 (GPIO17)
+// vinTube - boolean - on/off states for tubelight
+// vinBulb - boolean - on/off states for bulb
+                                                                                           
+int relayTubelight = D0; //pin to which relay is connected
+int relayBulb = D1;
+bool vinTube = HIGH; //a virtual boolean variable
+bool vinBulb = HIGH; //a virtual boolean variable
 
+// setup() is run only once, it's where we set up GPIO and initialise peripherals
 void setup() {
- pinMode(l1,OUTPUT);
- pinMode(l2,OUTPUT);
- pinMode(l3,OUTPUT);
- pinMode(s1,OUTPUT);
- pinMode(s2,OUTPUT);
- pinMode(s3,OUTPUT);
+    
+  // Setup GPIO
+  pinMode(relayTubelight,OUTPUT); // relay pin is set as output
+  digitalWrite(relayTubelight,LOW);
+  
+  pinMode(relayBulb,OUTPUT); // relay pin is set as output
+  digitalWrite(relayBulb,LOW);
+  // Subscribe to events published by IFTTT using Particle.subscribe
+  Particle.subscribe("eventTubeLightOn", tubeOffHandler); //turning tube light on function declaration
+  Particle.subscribe("eventTubeLightOff", tubeOnHandler); //turning tube light off function declaration
+  
+  Particle.subscribe("eventBulbOn", bulbOffHandler); //turning bulb on function declaration
+  Particle.subscribe("eventBulbOff", bulbOnHandler); //turning bulb off function declaration
 }
 
+// loop() runs continuously, it's our infinite loop.
 void loop() {
-  
+     if (vinTube==HIGH) {
+         digitalWrite(relayTubelight,LOW);
+     }
+else if (vinTube==LOW) {
+         digitalWrite(relayTubelight,HIGH);
+     }
+     if (vinBulb==HIGH) {
+         digitalWrite(relayBulb,LOW);
+     }
+else if (vinBulb==LOW) {
+         digitalWrite(relayBulb,HIGH);
+     }
+
+}
+
+//our events are called when IFTTT applets are triggered
+void tubeOffHandler(const char *event, const char *data) {
+    vinTube=LOW;
+}
+void tubeOnHandler(const char *event, const char *data) {
+     vinTube=HIGH;
+}
+
+void bulbOffHandler(const char *event, const char *data) {
+    vinBulb=LOW;
+}
+void bulbOnHandler(const char *event, const char *data) {
+     vinBulb=HIGH;
 }
